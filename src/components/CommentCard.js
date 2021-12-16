c569bbf372bc98def07eb347bdc2e0ec6591f0ea
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2021-12-16 11:45:14 +0700

    feat: delete comment

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,16 +1,37 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import {
   Card,
   CardActions,
   CardContent,
   Button,
-  Typography
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions
 } from '@mui/material';
 import { Delete } from '@mui/icons-material';
+import backend from '../api/backend';
 
+function CommentCard({ singleComment }) {
+  const { id, name, comment } = singleComment;
+  const [open, setOpen] = useState(false);
 
-export default function CommentCard({ singleComment }) {
-  const { name, comment } = singleComment;
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  const deleteComment = async () => {
+    await backend.delete(`/comments/destroy/${id}`);
+    setOpen(false);
+    window.location.reload();
+  }
+
   return (
     <Card>
       <CardContent>
@@ -22,8 +43,31 @@ export default function CommentCard({ singleComment }) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" startIcon={<Delete />}>Remove</Button>
+        <Button size="small" startIcon={<Delete />} onClick={handleClickOpen}>Remove</Button>
       </CardActions>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {`Delete comment by ${name}?`}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            This comment will be deleted permanently.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>No</Button>
+          <Button onClick={deleteComment} autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Card>
   );
 }
+
+export default CommentCard;
